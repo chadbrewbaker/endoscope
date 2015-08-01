42e3b1c07dc41dd526202e341ef15f2f323c5de7
Chad Brewbaker <crb002@gmail.com>
2015-08-01 15:16:40 -0400

    Spiking out more structure

--- Endoscope.hs
+++ Endoscope.hs
@@ -17,6 +17,31 @@ type Treelike = Bool
 --unranks :: Int -> String
 --unranks = undefined
 
+-- getIdempotents :: FunSet -> [a]
+
+-- getTreelikes :: FunSet -> [a]
+-- getLeaves :: FunSet -> [a]
+-- getGroupLikes :: Funset -> [a]
+
+
+-- getLolipopHistogram :: FunSet -> [ (IntCount,IntInex, IntPeriod)]
+
+-- getCompositionTable :: FunSet - > [ [IntFunID]] 
+
+
+-- gadgets :: (a -> a) -> [[a]]
+-- Returns the list of connected components in the function graph
+
+-- ordNarrowGadgets :: (a -> a) -> [[a]]
+-- Returns the list of connected components in the function graph in order of size where size < lg^{1/3} n
+
+-- wideGadgets :: (a -> a) -> [[a]]
+-- Returns the list of connected components in the function graph where size > lg^{1/3} n
+
+
+-- toSuccinct :: (a -> a) -> SuccinctFunction
+
+
 
 monogenic :: Int -> Int -> (Int -> Int -> Int) -> [Int]
 monogenic size seed mult = take size $ iterate f seed
@@ -45,6 +70,24 @@ idempotents size mult = concat [ (selectSame i)  | i <- [0.. size] ]
     where
        selectSame x |  (mult x x) == x  = [x]
                     | otherwise  = []
+
+
+
+-- Examples:
+-- Z2 Matrices | domain 2^{n*n}
+-- Z3 matrices | domain 3^{n*n}
+-- Life  | domain 2^{n*k}
+-- EndoFactors | domain 2^{n+k}
+--  Z_(k) +, * | domain n
+
+
+-- Two concepts: Succinct representation of a single function, monogenic graph over a set of functions
+
+
+
+
+
+
 
 main = do 
           print $ idempotents 7 mul7
