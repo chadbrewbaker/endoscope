2f0f788c094d967847bed2ee85baeefae268e9b9
Chad Brewbaker <crb002@gmail.com>
2015-11-15 11:19:00 -0600

    Bare bones working matmul Z2

---
+++ MatMul.hs
@@ -0,0 +1,27 @@
+
+import Data.List
+
+--toZ2 :: Num a => a -> a 
+toZ2 a =  mod a 2
+
+--mmult :: Num a => [[a]] -> [[a]] -> [[a]] 
+mmult a b = [ [ toZ2 $ sum $ zipWith (*) ar bc | bc <- transpose b ] | ar <- a ]
+
+idMat =[[1,0], 
+       [0, 1]]
+ 
+fibMat = [[1, 1],
+       [1, 0]]
+ 
+
+--powerListz2 ::Num a =>[[a]] -> [[a]]
+--powerListz2 [] = []
+--powerListz2 (x:xs) = (1:x) : (0:x) : (powerListz2 xs)
+
+--pListz2 :: Int -> [[Integer]]
+--pListz2 x = head.reverse $take x (iterate  powerListz2 [[]])
+
+--matsZ2 :: Int -> [[[Integer]]]
+--matsZ2 n =  replicateM n $ pListz2 (n+1)
+
+
