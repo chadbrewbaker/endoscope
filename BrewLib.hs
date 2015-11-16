617f8e7cb051bb6155a4c4f742df53ae6a3295c9
Chad Brewbaker <crb002@gmail.com>
2015-11-15 18:38:26 -0600

    got a transformation mult

--- BrewLib.hs
+++ BrewLib.hs
@@ -1,8 +1,10 @@
 module BrewLib where
 import Control.Monad
+import Data.Vector (toList, fromList, backpermute)
 
 powset = Control.Monad.filterM (const [True, False])
 trans x = replicateM (length x) x
 z2n n = replicateM n (replicateM n [0,1])
 
+transMult a b =  toList $backpermute (fromList a) (fromList b)
 
