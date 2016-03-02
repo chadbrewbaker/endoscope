fe713ee9e2ce007444081491db43da2f88cf409a
Brent Yorgey <byorgey@gmail.com>
2016-03-02 13:48:31 -0600

    a question

--- MatMul.hs
+++ MatMul.hs
@@ -1,13 +1,13 @@
 module MatMul where
 
-import Data.List
-import BrewLib
-import Control.Monad
+import           BrewLib
+import           Control.Monad
+import           Data.List
 
---toZ2 :: Num a => a -> a 
+--toZ2 :: Num a => a -> a
 toZ2 a =  mod a 2
 
---mmult :: Num a => [[a]] -> [[a]] -> [[a]] 
+--mmult :: Num a => [[a]] -> [[a]] -> [[a]]
 mmult a b = [ [ toZ2 $ sum $ zipWith (*) ar bc | bc <- transpose b ] | ar <- a ]
 
 madd a b = [ [ toZ2 $ product $ zipWith (+) ar bc | bc <- transpose b ] | ar <- a ]
@@ -17,6 +17,7 @@ powerListz2 ::Num a =>[[a]] -> [[a]]
 powerListz2 [] = []
 powerListz2 (x:xs) = (1:x) : (0:x) : powerListz2 xs
 
+-- Why not just pListz2 x = replicateM x [0,1]?  Does the order matter?
 pListz2 :: Int -> [[Integer]]
 pListz2 x = head.reverse $take x (iterate  powerListz2 [[]])
 
