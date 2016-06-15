685f86d9bb75295213664d4f1da8ff4c5a438696
Chad Brewbaker <crb002@gmail.com>
2016-06-15 15:42:25 -0500

    fixing tests

--- runtests.sh
+++ runtests.sh
@@ -2,9 +2,9 @@
 #make
 
 hlint ./
-runghc hasktests/Unit.hs
-#runghc hasktests/Profile.hs
-runghc hasktests/Check.hs
+runghc test/Unit.hs
+#runghc test/Profile.hs
+runghc test/Check.hs
 
 #ruby testendo.rb
 
