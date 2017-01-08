ca25de9f7c5964c5e0cc0698d3cf406616ec587c
Chad Brewbaker <crb002@gmail.com>
2017-01-08 02:37:04 -0600

    Adding generic mod and trans closure step to matmul

--- src/MatMul.hs
+++ src/MatMul.hs
@@ -6,15 +6,31 @@ import           Data.List
 
 --toZ2 :: Num a => a -> a
 toZ2 a =  mod a 2
+toZ2_close a | a > 1 = 1
+             | otherwise = a  
+toZn n a = mod a n
 
 --mmult :: Num a => [[a]] -> [[a]] -> [[a]]
 mmult a b = [ [ toZ2 $ sum $ zipWith (*) ar bc | bc <- transpose b ] | ar <- a ]
+mmult_close a b = [ [ toZ2_close $ sum $ zipWith (*) ar bc | bc <- transpose b ] | ar <- a ]
+mmultn a b n = [ [ toZn n $ sum $ zipWith (*) ar bc | bc <- transpose b ] | ar <- a ]
+
 madd a b = [ [ toZ2 $ product $ zipWith (+) ar bc | bc <- transpose b ] | ar <- a ]
+madd_close a b = [ [ toZ2_close $ product $ zipWith (+) ar bc | bc <- transpose b ] | ar <- a ]
+maddn a b n = [ [ toZn n $ product $ zipWith (+) ar bc | bc <- transpose b ] | ar <- a ]
 
 pListz2 :: Int -> [[Integer]]
 pListz2 x = replicateM x [0,1] 
 
 matsZ2 :: Int -> [[[Integer]]]
 matsZ2 n =  replicateM n $ pListz2 n
+
+pListz :: Int -> Int -> [[Integer]]
+pListz n x = replicateM x $ take n [0..] 
+
+matsZn :: Int -> Int -> [[[Integer]]]
+matsZn n k =  replicateM n $ pListz n k
+
+
 
 
