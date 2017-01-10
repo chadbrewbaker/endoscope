93200a4a29514b467be13e1294f101e451e632c1
Chad Brewbaker <crb002@gmail.com>
2017-01-09 19:58:00 -0600

    lens and trans

--- catgist.rb
+++ catgist.rb
@@ -1,12 +1,28 @@
 # Ways to transform functions so they can be used in a concatinative style
-
 class Proc
    def trans(*args)
-     newfunc =  ->(*a){
-       bound = [a.size, args.size].min
-       alist = (0...bound).collect{|i| a[args[i] ] }
-       self.call(alist)
-     }
+       ->(*a){
+           a.flatten!
+           bound = [a.size, args.size].min
+           alist = (0...bound).collect{|i| a[args[i] ] }
+           self.call(alist)
+       }
+   end
+   
+   def lens(*args)
+       concretes = [Integer,TrueClass,FalseClass,String,Float,Symbol,Array,Hash]
+       ->(*a){
+           a.flatten!
+           bound = [a.size, args.size].min
+           alist = (0...bound).collect{|i|
+               if(concretes.include?(args[i].class))
+                  args[i]
+                else
+                   args[i].call(a[i])
+               end
+           }
+           self.call(alist)
+       }
    end
 end
 
@@ -22,8 +38,17 @@ p g.call(0,1) == "[[nil, 0]]"
 p g.call(0,1,2) == "[[2, 0, 0]]"
 p g.call(0,1,2,3,4) == "[[2, 0, 0]]"
 
+single = ->(a){a}
+double = ->(a){a+a}
+tripple = ->(a){a+a+a}
+q = id.lens(single, double,tripple)
+p q.call(5,5,5) == "[[5, 10, 15]]"
+p q.call(1,2,3) == "[[1, 4, 9]]"
 
-
+h = q.lens(single,double,tripple)
+p h.call(5,5,5) == "[[5, 20, 45]]"
+k = id.lens(single, 444,tripple)
+p k.call(4,4,4,4,4) == "[[4, 444, 12]]"
 
 
 
